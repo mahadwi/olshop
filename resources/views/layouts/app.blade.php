ab821fa8a1aea36a4e8b6eaa7019af904bcfc71a
Slamet Riky Riyanto <riyanriky@gmail.com>
2023-10-12 07:24:50 +0700

    add sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,8 @@
             @include('layouts/navbar')
             <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
                 
+                @include('layouts/sidebar')
+
                 <div id="main-content" class="relative w-full h-full overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
                   <main>
                     @yield('content')
