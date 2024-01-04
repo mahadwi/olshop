df1069a99f4f9216f9cfb5f8111366978391539d
Slamet Riky Riyanto <riyanriky@gmail.com>
2024-01-04 10:59:45 +0700

    change method get ip location

--- app/Http/Controllers/API/LocationApiController.php
+++ app/Http/Controllers/API/LocationApiController.php
@@ -20,12 +20,28 @@ class LocationApiController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $ip = $request->getClientIp();
-        // $ip = '114.10.98.157';
+        
+        $ip = $this->getUserIpAddr();
 
         $location = Http::get($this->url.'?ip='.$ip)->json();
+        // $location = Http::get('https://api.ipify.org/?format=json')->json();
+
+        // dd($location);
 
         return $this->apiSuccess($location);
 
+    }
+
+    function getUserIpAddr(){
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+            //ip from share internet
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            //ip pass from proxy
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }else{
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+        return $ip;
     }
 }
