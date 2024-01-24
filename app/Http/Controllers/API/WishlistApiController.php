97b0802d1c7dc52a93fbd2a8554d8bc0a3de23c6
Slamet Riky Riyanto <riyanriky@gmail.com>
2024-01-24 17:04:34 +0700

    wishlist pagination, filter

--- app/Http/Controllers/API/WishlistApiController.php
+++ app/Http/Controllers/API/WishlistApiController.php
@@ -11,13 +11,52 @@ use Illuminate\Http\Request;
 class WishlistApiController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        $data = Wishlist::where('user_id', auth()->user()->id)->get();
+        $query = Wishlist::where('user_id', auth()->user()->id)
+        ->when($request->has('search'), function ($query) use ($request) {
+            $query->whereHas('product', function ($query) use ($request){
+                $query->where('name', 'ilike', "%{$request->search}%");
+            });            
+        });
+        
+        if ($request->has('page')) {
+            if ($request->has('itemPerpage')) {
+                $query = $query->paginate($request->has('itemPerpage'));
+            } else {
+                $query = $query->paginate(10);
+            }
+        } else {
+            $query = $query->get();
+        }
 
-        $wishlist = fractal($data, new WishListTransformer)->toArray();
+        if ($request->has('page')) {
+            $wishlist = fractal($query->items(), new WishListTransformer($request->all()))->toArray();
+        } else {
+            $wishlist = fractal($query, new WishListTransformer($request->all()))->toArray();
+        }
 
-        return $this->apiSuccess($wishlist);
+        $wishlist = collect($wishlist);
+
+        $wishlistF = [];
+
+        foreach ($wishlist as $dataProdct) {
+            $wishlistF[] = $dataProdct;
+        }
+
+        $meta = [];
+        if (isset($request->page)) {
+            $meta = [
+                'currentPage' => $query->currentPage(),
+                'itemsPerPage' => $query->perPage(),
+                'nextPage' => $query->currentPage() < $query->lastPage() ? $query->currentPage() + 1 : null,
+                'totalItemCount' => $query->count(),
+                'totalPage' => $query->lastPage(),
+                'lastPage' => $query->lastPage(),
+                'totalItem' => $query->total()
+            ];
+        }
+        return $this->apiSuccess($wishlistF, $meta);
     }
 
     public function store(StoreWishlistApiRequest $request)
