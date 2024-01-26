256691fa7a990e4ba045d2cddaa99641c8ede622
Slamet Riky Riyanto <riyanriky@gmail.com>
2024-01-26 15:00:19 +0700

    set required update vendor

--- app/Http/Requests/API/UpdateVendorApiRequest.php
+++ app/Http/Requests/API/UpdateVendorApiRequest.php
@@ -22,14 +22,14 @@ class UpdateVendorApiRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name'                  => 'nullable|string|max:255|unique:vendors,name,' . $this->vendor->id,
-            'phone'                 => 'nullable|string|min:9|max:16|unique:vendors,phone,' . $this->vendor->id,  
-            'email'                 => 'nullable|string|email|max:255|unique:vendors,email,' . $this->vendor->id,  
-            'ktp'                   => 'nullable|integer',
-            'bank'                  => 'nullable|exists:bank_codes,code',  
-            'bank_account_holder'   => 'nullable|string',
-            'bank_account_number'   => 'nullable|integer',     
-            'address'               => 'nullable|string',     
+            'name'                  => 'required|string|max:255|unique:vendors,name,' . $this->vendor->id,
+            'phone'                 => 'required|string|min:9|max:16|unique:vendors,phone,' . $this->vendor->id,  
+            'email'                 => 'required|string|email|max:255|unique:vendors,email,' . $this->vendor->id,  
+            'ktp'                   => 'required|integer',
+            'bank'                  => 'required|exists:bank_codes,code',  
+            'bank_account_holder'   => 'required|string',
+            'bank_account_number'   => 'required|integer',     
+            'address'               => 'required|string',     
         ];
     }
 }
