d84a65bcd627fb2edff439b8589caa836241e476
Slamet Riky Riyanto <riyanriky@gmail.com>
2024-02-23 16:21:25 +0700

    fix 6 digit otp

--- app/Actions/API/StoreOtpAction.php
+++ app/Actions/API/StoreOtpAction.php
@@ -12,7 +12,7 @@ class StoreOtpAction
     public function handle(User $user)
     {
         return DB::transaction(function () use ($user) {
-            $otp = rand(000000, 999999);
+            $otp = str_pad(rand(0, 999999), 6, '0', STR_PAD_LEFT);
             $user->otp = $otp;
             $user->otp_expired_at = Carbon::now()->addSeconds(config('app.default.otp_lifetime'));
             $user->otp_next_try = Carbon::now()->addSeconds(config('app.default.otp_next_try'));
